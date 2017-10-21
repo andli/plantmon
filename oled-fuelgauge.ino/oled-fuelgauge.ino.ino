3d52eb00e3d3de7cc8a8b681ca3800d46bbb12c2
Andreas <a.ehrlund@gmail.com>
2017-10-21 20:31:31 +0200

    improvements with display timeout

--- oled-fuelgauge.ino/oled-fuelgauge.ino.ino
+++ oled-fuelgauge.ino/oled-fuelgauge.ino.ino
@@ -22,21 +22,35 @@ LiFuelGauge gauge(MAX17043, 0, lowPower);
 
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);
 
-unsigned long time;
+unsigned long lastPushTime;
+unsigned long lastMeasureTime;
 const int SCREEN_HEIGHT = 32;
 const int SCREEN_WIDTH = 128;
 const int NUM_MEASUREMENTS = 12;
-const long READ_INTERVAL = 1000L * 60L * 5L / 120L; //TODO: remove 120L here
+const long LOOP_INTERVAL = 250L;
+const long READ_INTERVAL = 1000L * 6; //0L * 5L / 120L; //TODO: remove 12L here
+const long DISPLAY_TIMEOUT = 1000L * 15L;
 
 const int analogInPin = A0;
-const int transistorPin = 4;
+const int transistorMeasurePin = 4;
+const int pushbuttonPin = 3;
 int analogInValue = 0;
+bool renderScreen = true;
 
 int y[NUM_MEASUREMENTS];
 
 void setup(void) {
-  pinMode(transistorPin, OUTPUT);
+  Serial.begin(9600);
+  
+  pinMode(pushbuttonPin, INPUT);    
+  pinMode(transistorMeasurePin, OUTPUT);
+  
+  lastPushTime = 0L;
+  lastMeasureTime = 0L;
+  
   u8g2.begin();
+
+  u8g2.setFont(u8g2_font_profont11_tn);
 
   gauge.reset();  // Resets MAX17043
   delay(200);  // Waits for the initial measurements to be made
@@ -73,38 +87,49 @@ void drawBattery(int screenHeight, int batteryWidth, int batteryLevelHeight) {
 }
 
 void measureMoisture() {
-  digitalWrite(transistorPin, HIGH);
+  digitalWrite(transistorMeasurePin, HIGH);
   delay(500);
   analogInValue = analogRead(analogInPin);
-  digitalWrite(transistorPin, LOW);
+  digitalWrite(transistorMeasurePin, LOW);
 
   for (int k = NUM_MEASUREMENTS; k >= 0; k--) {
     y[k] = y[k - 1];
   }
   y[0] = map(analogInValue, 0, 1023, 0, SCREEN_HEIGHT);
-  //y[0] = map((int)gauge.getSOC(), 0, 100, 0, SCREEN_HEIGHT);
 }
 
 void loop(void) {
-  time = millis();
-  //if (y[0] < 1 || time > 1000L * 60L * 60L) {
-  measureMoisture();
-  time = 0L;
-  //}
 
+  if (millis() - lastPushTime > DISPLAY_TIMEOUT && renderScreen) {
+    Serial.println("display timeout");
+    renderScreen = false;
+  }
+
+  if (digitalRead(pushbuttonPin) == HIGH && !renderScreen) {
+    Serial.println("button pushed");
+    lastPushTime = millis();
+    renderScreen = true;
+  }
+
+  if (millis() - lastMeasureTime > READ_INTERVAL) {
+    measureMoisture();
+    lastMeasureTime = millis();
+  }
+  
   u8g2.firstPage();
-  do {
-    u8g2.setFont(u8g_font_unifont);
-    drawBattery(32, 10, 26);
-    drawMeasurements(15, 0, SCREEN_WIDTH - 15, SCREEN_HEIGHT);
-    char result[8]; // Buffer big enough for 7-character float
-    //dtostrf(gauge.getVoltage(), 6, 2, result); // Leave room for too large numbers!
-    itoa(freeRam(), result, 10);
-    u8g2.drawStr(12, 16, result);
-    dtostrf(gauge.getSOC(), 6, 2, result); // Leave room for too large numbers!
-    u8g2.drawStr(12, 32, result);
-  } while ( u8g2.nextPage() );
+  if (renderScreen) {
+    do {
+      drawBattery(32, 10, 26);
+      drawMeasurements(15, 0, SCREEN_WIDTH - 15, SCREEN_HEIGHT);
+      char result[8]; // Buffer big enough for 7-character float
+      dtostrf(gauge.getSOC(), 6, 2, result); // Leave room for too large numbers!
+      //itoa(freeRam(), result, 10);
+      u8g2.drawStr(12, 8, result);
+      //dtostrf(gauge.getVoltage(), 6, 2, result); // Leave room for too large numbers!
+      //u8g2.drawStr(12, 32, result);
+    } while ( u8g2.nextPage() );
+  }
 
   // rebuild the picture after some delay
-  delay(READ_INTERVAL);
+  delay(LOOP_INTERVAL);
 }
